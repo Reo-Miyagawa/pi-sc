d4fe9adeb0a5def94c5791c04848a428d9343a58
Reo-Miyagawa <atethxhrimatggg@gmail.com>
2020-12-18 22:24:52 +0900

    ストロングパラメーターへユーザー情報の追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,8 +4,22 @@ class ListsController < ApplicationController
   end
 
   def new
+    @lists = List.new
   end
 
   def create
+    @lists = List.create(list_params)
+    if @lists.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title).merge(user: current_user)
+  end
+
 end
