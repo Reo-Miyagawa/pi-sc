9d1a93c612d8d32cfba4d74930f89f572510517b
Reo-Miyagawa <atethxhrimatggg@gmail.com>
2020-12-18 18:49:49 +0900

    newルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'pi_scs#index'
+  resources :pi_scs, only: [:new]
 end
