333473c302d150fcad90a2976a792ee5d148bacc
Reo-Miyagawa <atethxhrimatggg@gmail.com>
2020-12-18 18:43:27 +0900

    マイグレーションとモデル作成

--- db/migrate/20201218093713_create_lists.rb
+++ db/migrate/20201218093713_create_lists.rb
@@ -2,6 +2,8 @@ class CreateLists < ActiveRecord::Migration[6.0]
   def change
     create_table :lists do |t|
 
+      t.string      :title,  null: false
+      t.references  :user,   foreign_key: true
       t.timestamps
     end
   end
