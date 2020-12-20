6dcea3274113ab64d6d166b941ccc0be9d03a8b7
Reo-Miyagawa <atethxhrimatggg@gmail.com>
2020-12-20 11:08:30 +0900

    editルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'lists#index'
-  resources :lists, only: [:new, :create]
+  resources :lists, only: [:new, :create, :edit]
 end
